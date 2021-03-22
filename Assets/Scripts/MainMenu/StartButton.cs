56b5e0edf06a23f50c4bbcc98d53f0100ba42a08
5tarlight <developerstarlight@gmail.com>
2021-03-22 17:15:56 +0900

    start button function with scene changer

---
+++ Assets/Scripts/MainMenu/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Snake.MainMenu
+{
+  public class StartButton : MonoBehaviour
+  {
+    public void OnClick()
+    {
+      SceneManager.LoadScene("Game");
+    }
+  }
+}
